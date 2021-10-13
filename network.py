e4e293fd83ad80ad4d26d2f2a8b1193fb210a4ab
das08 <takeda5955@gmail.com>
2021-10-13 19:52:12 +0900

    ユーザーの情報を取得する

--- network.py
+++ network.py
@@ -1,7 +1,10 @@
 import requests
+import datetime
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+
+from model import UserData, ApexUser
 
 load_dotenv(verbose=True)
 
@@ -9,10 +12,16 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 
-def fetchUserData(platform: str, uid: str):
+def fetchUserData(au: ApexUser) -> dict:
     API_KEY = os.environ.get("API_KEY")
-    res = requests.get(f"https://public-api.tracker.gg/v2/apex/standard/profile/{platform}/{uid}?TRN-Api-Key={API_KEY}")
+    res = requests.get(f"https://public-api.tracker.gg/v2/apex/standard/profile/{au.platform}/{au.uid}?TRN-Api-Key={API_KEY}")
+
+    if res.status_code != 200:
+        return {"status": res.status_code, "user_data": None}
 
     response = res.json()
+
+    timestamp = int(round(datetime.datetime.now().timestamp()))
+    return {"status": res.status_code, "user_data": UserData(au, int(response["data"]["segments"][0]["stats"]["level"]["value"]), timestamp)}
 
 
